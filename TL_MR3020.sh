2103a92f1a2c0ad4468da6baca2897ab4ae5fef8
Andrew Aladjev <aladjev.andrew@gmail.com>
2013-11-17 00:45:26 +0300

    added openssh-client

--- TL_MR3020.sh
+++ TL_MR3020.sh
@@ -8,5 +8,5 @@ nano \
 usb-modeswitch usb-modeswitch-data \
 comgt \
 kmod-usb-ohci kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan \
--dropbear openssh-server openssh-sftp-server \
+-dropbear openssh-server openssh-sftp-server openssh-client \
 "
