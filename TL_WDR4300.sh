f58dfd09455eb60f60e905b8aee7ac8155b03b51
andrew-aladev <aladjev.andrew@gmail.com>
2013-11-17 00:55:05 +0300

    added openssh-client

--- TL_WDR4300.sh
+++ TL_WDR4300.sh
@@ -4,7 +4,7 @@ make image PROFILE="TLWDR4300" PACKAGES=" \
 -opkg \
 sudo \
 nano \
--dropbear openssh-server openssh-sftp-server sshfs \
+-dropbear openssh-server openssh-sftp-server openssh-client sshfs \
 openvpn \
 block-mount kmod-usb-storage kmod-fs-ext4 \
 "
