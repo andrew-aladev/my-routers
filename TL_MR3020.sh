54885806846e8ba6ea718b810f7ad8e0e5471e6e
andrew-aladev <aladjev.andrew@gmail.com>
2013-11-16 22:37:14 +0300

    added sshfs

--- TL_MR3020.sh
+++ TL_MR3020.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 make image PROFILE="TLMR3020" PACKAGES=" \
--opkg -ppp-mod-pppoe \
+-opkg \
+-ppp-mod-pppoe \
+sudo \
 nano \
 usb-modeswitch usb-modeswitch-data \
 comgt \
 kmod-usb-ohci kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan \
--dropbear openssh-server openssh-sftp-server \
-sudo \
+-dropbear openssh-server openssh-sftp-server sshfs \
 "
