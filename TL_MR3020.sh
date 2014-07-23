7516b313ec5bcb43beed255661ba53f2be8cf155
Andrew Aladjev <aladjev.andrew@gmail.com>
2014-07-23 12:10:59 +0300

    added openvpn to mr 3020

--- TL_MR3020.sh
+++ TL_MR3020.sh
@@ -4,9 +4,9 @@ make image PROFILE="TLMR3020" PACKAGES=" \
 -opkg \
 -ppp-mod-pppoe \
 sudo \
-nano \
 usb-modeswitch usb-modeswitch-data \
 comgt \
 kmod-usb-ohci kmod-usb-serial kmod-usb-serial-option kmod-usb-serial-wwan \
--dropbear openssh-server openssh-sftp-server openssh-client \
+-dropbear openssh-server openssh-client \
+openvpn \
 "
