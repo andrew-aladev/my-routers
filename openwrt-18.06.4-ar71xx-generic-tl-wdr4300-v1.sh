8470b76a8d8b2cfed6d651b881abb6cb640148ca
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-07-21 02:04:13 +0300

    returned back dropbear is enough stable

--- openwrt-18.06.4-ar71xx-generic-tl-wdr4300-v1.sh
+++ openwrt-18.06.4-ar71xx-generic-tl-wdr4300-v1.sh
@@ -3,7 +3,6 @@
 make image PROFILE="tl-wdr4300-v1" PACKAGES=" \
 -opkg \
 sudo \
--dropbear openssh-server openssh-sftp-server openssh-client sshfs \
 block-mount kmod-usb-storage kmod-fs-ext4 \
 openvpn-openssl \
 nano \
